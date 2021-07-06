371fb49a66ddbeca798a26c8448889b173a7f19f
tinytoots <wangchen2018n@163.com>
2021-07-05 22:04:39 -0400

    state and props practice

--- frontend/todo-app/src/components/CounterBackUp/counterBackup.js
+++ frontend/todo-app/src/components/CounterBackUp/counterBackup.js
@@ -3,33 +3,12 @@ import './Counter.css'
 import PropTypes from 'prop-types'
 
 export default class Counter extends Component {
-
-    // Define the initial state in a constructor
-    // state => counter 0
-    constructor() {
-        super(); // Error 1 使用this就需要super
-
-        this.state = {
-            counter : 0,
-        }
-
-        this.increment = this.increment.bind(this);
-    }
-
-    increment(by) { 
-        // console.log(`increment from parent - ${by}`)
-        this.setState({
-            counter: this.state.counter + by
-        });
-    }
-
     render() {
         return (
           <div className="Counter">
-            <CounterButton by={1} incrementMethod={this.increment}/> 
-            <CounterButton by={5} incrementMethod={this.increment}/> 
-            <CounterButton by={10} incrementMethod={this.increment}/> 
-            <span className="count">{this.state.counter}</span>
+            <CounterButton by={1}/> 
+            <CounterButton by={5}/> 
+            <CounterButton by={10}/> 
           </div>
         );
       } 
@@ -44,21 +23,25 @@ class CounterButton extends Component {
 
         this.state = {
             counter : 0,
+            secondCounter : 100
         }
 
+        // bind the method to the class 如果不想写这个bind，需要把render写出箭头函数
         this.increment = this.increment.bind(this);
+
     }
 
 
-    increment() { 
+    // update state - counter++ 如果在class里用方法就需要删除前面的function
+    increment() { // 如果使用箭头函数这里也要改箭头函数
         // console.log('increment'); 
+        // this.state.counter++;  改变state不能直接改，必须要使用setState改
         this.setState({
             counter: this.state.counter + this.props.by,
         });
-
-        this.props.incrementMethod(this.props.by);
     }
     
+    // 不写箭头函数上面就需要bind
     render() {
         return (
             <div className='counter'> 
