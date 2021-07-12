dd73c1ebd4da1568a5d220fabdda9bf1cded2bf1
tinytoots <wangchen2018n@163.com>
2021-07-11 22:10:59 -0400

    add login page

--- frontend/todo-app/src/components/todo/TodoApp.js
+++ frontend/todo-app/src/components/todo/TodoApp.js
@@ -17,30 +17,83 @@ class LoginComponent extends Component {
         super(props)
         this.state = {
             username: 'chenwang',
-            password: ''
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false,
         }
-        this.handleUsernameChange = this.handleUsernameChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+    }
+
+    handleChange(event) {
+        console.log(this.state);
+        this.setState(
+            {
+                [event.target.name]
+                    :event.target.value
+            }
+        )
     }
 
     // in terms of HTML, any action that you perform in the button, you click it, you generate an event in React
-    handleUsernameChange(event) {
-        console.log(event.target.value);
-        this.setState({username:event.target.value})
-    }
+    // handleUsernameChange(event) {
+    //     console.log(event.target.name);
+    //     this.setState(
+    //         {
+    //             [event.target.name]
+    //                 :event.target.value
+    //         }
+    //     )
+    // }
 
-    handlePasswordChange(event) {
-        console.log(event.target.value);
-        this.setState({password:event.target.value})
+    // handlePasswordChange(event) {
+    //     console.log(event.target.value);
+    //     this.setState({password:event.target.value})
+    // }
+
+    loginClicked() {
+        // chenwang, good
+        if (this.state.username === 'chenwang' && this.state.password === 'good') {
+            console.log('Successful')
+            this.setState({showSuccessMessage:true})
+            this.setState({hasLoginFailed:false})
+        } else {
+            console.log('Failed')
+            this.setState({showSuccessMessage:false})
+            this.setState({hasLoginFailed:true})
+        }
+        console.log(this.state)
     }
 
     render() {
         return (
             <div>
-                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
-                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/> 
-                <button>Login</button>
+                {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} /> */}
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
+                {this.state.showSuccessMessage && <div>Login Successful</div>}
+                {/* <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage} /> */}
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/> 
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         )
     }
 }
+
+// function ShowInvalidCredentials(props){
+//     if (props.hasLoginFailed) {
+//         return <div>Invalid Credentials</div>
+//     } else {
+//         return null
+//     }
+// }
+
+// function ShowLoginSuccessMessage(props) {
+//     if (props.showSuccessMessage) {
+//         return <div>Login Successful</div>
+//     } else {
+//         return null
+//     }
+// }
