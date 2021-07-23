054912188de8b26e9222b50a2422a8e1bc5f4d81
tinytoots <wangchen2018n@163.com>
2021-07-23 00:52:10 -0400

    axios paractice

--- frontend/todo-app/src/api/todo/HellowWorldService.js
+++ frontend/todo-app/src/api/todo/HellowWorldService.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 
 class HellowWorldService {
+
     executeHelloWorldService() {
         // console.log('executed service')
         return axios.get('http://localhost:8080/hello-world');
     }  
+
+    executeHelloWorldBeanService() {
+        // console.log('executed service')
+        return axios.get('http://localhost:8080/hello-world-bean');
+    }  
+
+    executeHelloWorldPathVariableService(name) {
+        // console.log('executed service')
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`);
+    }  
+
 }
 
 export default new HellowWorldService();
--- frontend/todo-app/src/components/todo/WelcomeComponent.js
+++ frontend/todo-app/src/components/todo/WelcomeComponent.js
@@ -6,6 +6,10 @@ export default class WelcomeComponent extends Component {
     constructor(props) {
         super(props) 
         this.retriveWelcomeMessage = this.retriveWelcomeMessage.bind(this)
+        this.state = {
+            welcomeMessage : ''
+        }
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
     }
 
     render() {
@@ -14,24 +18,38 @@ export default class WelcomeComponent extends Component {
                 <h1>Welcome!</h1>
                 <div className="container">
                     <div>
+                        {/* name是从TodoApp.js里的Route那里得到的param */}
                         Welcome {this.props.match.params.name}. 
                         You can manage your todos <Link to="/todos">here</Link>.
                     </div>
                 </div>
                 <div className="container">
-                    <div>
-                        Click here to get a customized welcome message.    
-                        You can manage your todos <Link to="/todos">here</Link>.
-                        <button onClick={this.retriveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
-                    </div>
+                    Click here to get a customized welcome message.    
+                    You can manage your todos <Link to="/todos">here</Link>.
+                    <button onClick={this.retriveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
                 </div>
             </div>
         )
     }
 
     retriveWelcomeMessage() {
-        HellowWorldService.executeHelloWorldService()
-        .then(response => console.log(response))
+        // HellowWorldService.executeHelloWorldService()
+        // .then(response => this.handleSuccessfulResponse(response))
+
+        // HellowWorldService.executeHelloWorldBeanService()
+        // .then(response => this.handleSuccessfulResponse(response))
+
+        HellowWorldService.executeHelloWorldPathVariableService(this.props.match.params.name)
+        .then(response => this.handleSuccessfulResponse(response))
+    }
+
+    handleSuccessfulResponse(response) {       
+        console.log(response)
+        // you cannot display an object directly, that's why we use response.data.message here
+        this.setState({welcomeMessage: response.data.message})
     }
 
 }
